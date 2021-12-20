c6153427731e6e2c6cad3037fceacf1ed6d4f7f7
Youngjun Buzz Seo <91406724+0dyuni@users.noreply.github.com>
2021-12-20 20:22:15 +0900

    Deleted webSocket, replaced with socket.io

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import http from "http";
-import WebSocket from "ws";
+// import WebSocket from "ws";
+import SocketIo from "socket.io";
 import express from "express";
+import { Socket } from "dgram";
 
 const app = express();
 
@@ -10,33 +12,37 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`연결 http://localhost:3030`);
+const httpServer = http.createServer(app);
+const wsSever = SocketIo(httpServer);
+// const wss = new WebSocket.Server({ server });
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
-
-function onSocketClose() {
-  console.log("브라우져와 연결 끊김 ❌");
-}
-
-const sockets = [];
-
-wss.on("connection", (socket) => {
-  sockets.push(socket);
-  socket["nickname"] = "익명";
-  console.log("브라우져와 연결 ✅");
-  socket.on("close", onSocketClose);
-  socket.on("message", (msg) => {
-    const message = JSON.parse(msg);
-    switch (message.type) {
-      case "new_message":
-        sockets.forEach((aSocket) =>
-          aSocket.send(`${socket.nickname}: ${message.payload}`)
-        );
-      case "nickname":
-        socket["nickname"] = message.payload;
-    }
-  });
+wsSever.on("connection", (socket) => {
+  console.log(socket);
 });
 
-server.listen(3030, handleListen);
+// function onSocketClose() {
+//   console.log("브라우져와 연결 끊김 ❌");
+// }
+
+// const sockets = [];
+
+// wss.on("connection", (socket) => {
+//   sockets.push(socket);
+//   socket["nickname"] = "익명";
+//   console.log("브라우져와 연결 ✅");
+//   socket.on("close", onSocketClose);
+//   socket.on("message", (msg) => {
+//     const message = JSON.parse(msg);
+//     switch (message.type) {
+//       case "new_message":
+//         sockets.forEach((aSocket) =>
+//           aSocket.send(`${socket.nickname}: ${message.payload}`)
+//         );
+//       case "nickname":
+//         socket["nickname"] = message.payload;
+//     }
+//   });
+// });
+
+const handleListen = () => console.log(`연결 http://localhost:3030`);
+httpServer.listen(3030, handleListen);
